f9935ad5c481f8dda0d7c8ce8d7e0fe58d534aa4
Bawantha Madhushan <153340507+BMadhushan18@users.noreply.github.com>
2025-04-05 09:11:32 +0530

    App.js was changed by bawantha

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors"); 
+
+
+//Tharinda ................................................................
 const roomRoutes = require("./routes/Tharinda_routes/roomRoutes"); 
-// const bookingRoutes = require("./routes/Tharinda_routes/bookingRoutes");
-// const bookingHistoryRoutes = require("./routes/Tharinda_routes/bookingHistoryRoutes");
+
 const app = express();
 
 // Middleware
@@ -14,14 +16,37 @@ console.log(" App.js Loaded");
 
 // Define API Routes
 app.use("/api/rooms", roomRoutes); 
-// app.use("/api/bookings", bookingRoutes);
+
+
+
+//Bawantha......................................................................
+
+const bookingRoutes = require("./routes/Bawantha_routes/bookingRoutes");
+const bookingHistoryRoutes = require("./routes/Bawantha_routes/bookingHistoryRoutes");
+
+// Define API Routes
+
+app.use("/api/bookings", bookingRoutes);
 
 // booking history
-// app.use("/api/booking-history", bookingHistoryRoutes);
+app.use("/api/booking-history", bookingHistoryRoutes);
+
 
 // Default Route for Testing
 app.get("/", (req, res) => {
   res.send("Hotel Booking API is Running!");
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
