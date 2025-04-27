c14ac1a304cfbe949e11820f96358180e975694f
Bawantha Madhushan <153340507+BMadhushan18@users.noreply.github.com>
2025-04-27 22:31:59 +0530

    Checkout button was added

--- frontend/src/components/Bawantha_components/AvailableRoomsTable.js
+++ frontend/src/components/Bawantha_components/AvailableRoomsTable.js
@@ -13,16 +13,16 @@ const AvailableRoomsTable = ({ rooms }) => {
 
   return (
     <div className="overflow-x-auto">
-      <h3 className="text-xl font-semibold mb-5">Available Rooms</h3>
       <table className="w-full border-collapse border border-gray-300">
         <thead>
           <tr className="bg-gray-200 text-center">
-            <th className="border p-2 w-1/6">Room Number</th>
+            <th className="border p-2 w-1/6">Room</th>
             <th className="border p-2 w-1/6">Room Type</th>
             <th className="border p-2 w-1/6">Bed</th>
             <th className="border p-2 w-1/6">Price (Rs)</th>
             <th className="border p-2 w-1/6">Size</th>
-            <th className="border p-2 w-1/6">Action</th>
+            <th className="border p-2 w-1/6">Booking</th>
+            <th className="border p-2 w-1/6">Checkout</th>
           </tr>
         </thead>
         <tbody>
@@ -36,17 +36,25 @@ const AvailableRoomsTable = ({ rooms }) => {
                 <td className="border p-2">{room.size}</td>
                 <td className="border p-2">
                   <button
-                    className="bg-green-500 text-white shadow-md rounded-md px-3 py-1 hover:bg-green-600 transition w-full"
+                    className="bg-green-500 text-white shadow-md rounded-md px-3 py-1 hover:bg-green-600 transition"
                     onClick={() => navigate(`/booking/${room.roomNumber}`)}
                   >
                     Booking
+                  </button>
+                </td>
+                <td className="border p-2">
+                  <button
+                    className="bg-blue-500 text-white shadow-md rounded-md px-3 py-1 hover:bg-blue-600 transition"
+                    onClick={() => navigate(`/checkout/${room.roomNumber}`)}
+                  >
+                    Checkout
                   </button>
                 </td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="6" className="text-center p-4">
+              <td colSpan="7" className="text-center p-4">
                 No rooms available
               </td>
             </tr>
